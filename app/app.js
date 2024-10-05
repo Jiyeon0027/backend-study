a06716ebf81774f9b183a36034726df20eed343d
jiyeon <rlawldus3412@gmail.com>
2024-10-05 15:18:20 +0900

    Add post api method

--- app/app.js
+++ app/app.js
@@ -2,18 +2,20 @@
 
 //module
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+//middleware (라우터보다 먼저 정의)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //routing
 const home = require("./src/routes/home");
-
-//views
-
-app.set("views", "./src/views");
-app.set("view engine", "ejs");
-
-//middleware
 app.use("/", home);
 app.use(express.static(`${__dirname}/src/public`));
+
+//views
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
 
 module.exports = app;
